27b24dce8d82dd3f7b5bdf92e88d30adb6e97ad1
evanmiyakawa <evanmiyakawa@gmail.com>
2019-07-04 11:05:35 -0500

    print bare_mpoly if mpoly is length 1

--- R/print.mpoly.R
+++ R/print.mpoly.R
@@ -1,20 +1,28 @@
-# 
+# print mpoly
 
 print.mpoly <- function (x, silent = FALSE, ...) {
   
-  # make printouts of bare_mpolys
-  printed_strings <- vapply(x, print.bare_mpoly, character(1), silent = TRUE)
-  
-  # add back dimensions (if dimensionless, assumed to be a column/row vector)
-  if (!is.null(dim(x))) dim(printed_strings) <- dim(x)
-  
-  # replace "'s with spaces
-  printed_strings <- capture.output(print(printed_strings))
-  printed_strings <- str_replace_all(printed_strings, "(?<= )\"", "")
-  printed_strings <- str_replace_all(printed_strings, "\"(?= |$)", "  ")
-  
-  # print/return
-  if(!silent) cat(printed_strings, sep = "\n")
-  invisible(printed_strings)
+  if (length(x) == 1) {
+    # if mpoly is bare_mpoly, use print.bare_mpoly
+    print.bare_mpoly(x[[1]])
+    invisible(x[[1]])
+  } 
+  else {
+    # make printouts of bare_mpolys
+    printed_strings <- vapply(x, print.bare_mpoly, character(1), silent = TRUE)
+    
+    # add back dimensions (if dimensionless, assumed to be a column/row vector)
+    if (!is.null(dim(x))) dim(printed_strings) <- dim(x)
+    
+    # replace "'s with spaces
+    printed_strings <- capture.output(print(printed_strings))
+    printed_strings <- str_replace_all(printed_strings, "(?<= )\"", "")
+    printed_strings <- str_replace_all(printed_strings, "\"(?= |$)", "  ")
+    
+    # print/return
+    if(!silent) cat(printed_strings, sep = "\n")
+    invisible(printed_strings)
+  }
   
 }
+
